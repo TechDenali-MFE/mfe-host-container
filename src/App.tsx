41a50766facd1b60e7602bc21a8ba1ceb3676aae
Geethika K <geethika.kotramangalam@techdenali.com>
2026-06-22 13:02:44 +0530

    Added geethika's widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { loadWidget, removeWidget } from "./utils/widgetLoader";
 
 const USER_WIDGET_URL = import.meta.env.VITE_TEST_WIDGET_URL;
-
+const USER_WIDGET_URL1 = import.meta.env.VITE_TEST_WIDGET_URL1;
 function App() {
   return (
     <>
@@ -17,17 +17,32 @@ function App() {
         Load User Widget
       </button>
 
+      <div id="user-widget"></div>
+
+
+<div>
+ <button
+        onClick={() =>
+          loadWidget(USER_WIDGET_URL1, "user-widget1", {
+            name: "Geethika",
+          })
+        }
+      >
+        Load User geethika'sWidget
+      </button>
+
       <button
-        onClick={() => removeWidget("user-widget")}
+        onClick={() => removeWidget("user-widget1")}
       >
         Remove User Widget
       </button>
-
+       <div id="user-widget1"></div>
+</div>
       <hr />
 
-      <div id="user-widget"></div>
 
-      
+
+         
     </>
   );
 }
